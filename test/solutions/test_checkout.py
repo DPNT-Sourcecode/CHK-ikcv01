52e63f4b2793a59caa7e7cc6d097bb79dc60f2a1
sbx_user1059 <sbx_user1059@ip-10-12-7-129.eu-west-2.compute.internal>
2018-05-13 21:51:47 +0000

    Sun May 13 21:51:47 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -5,14 +5,11 @@ from lib.solutions.checkout import checkout
 
 class TestCheckout(unittest.TestCase):
     def test_checkoutFormat(self):
-        self.assertEqual(checkout('A3B1'), -1)
-        self.assertEqual(checkout('ACBA'), -1)
-        self.assertEqual(checkout('A*3B'), -1)
-        self.assertEqual(checkout('A*3'), -1)
-        self.assertEqual(checkout('A*1,B*2'), -1)
         self.assertEqual(checkout(''), 0)
         self.assertEqual(checkout('A'), 50)
         self.assertEqual(checkout('B'), 30)
+        self.assertEqual(checkout('C'), 20)
+        self.assertEqual(checkout('D'), 15)
 
 if __name__ == '__main__':
     unittest.main()
