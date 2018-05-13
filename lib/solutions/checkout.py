7d4bcfd544bbe996364e874f7267f5fdbbd764b7
sbx_user1059 <sbx_user1059@ip-10-12-7-129.eu-west-2.compute.internal>
2018-05-13 21:21:36 +0000

    Sun May 13 21:21:36 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -3,4 +3,16 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    """
+    I am assuming products are sepereted by commas
+    and their quantity follows as an int sperated with an asterisk
+    e.g. skus="A*3,B*1"
+    the server tests will reveal the input string's format after the penalty
+    I also have no information on the offers for multiple items
+    """
+    try: # let's see if the string format guess was correct
+        products = skus.split(',') # split products
+        for p in products: # split the quantities
+            p.split('*')
+    except:
+        return -1
