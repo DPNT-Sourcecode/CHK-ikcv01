35bc9b52281ad6950f0654bd2a2006870a9da91c
sbx_user1059 <sbx_user1059@ip-10-12-7-129.eu-west-2.compute.internal>
2018-05-13 21:37:01 +0000

    Sun May 13 21:37:01 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -8,8 +8,10 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(checkout('A'), -1)
         self.assertEqual(checkout('A3B1'), -1)
         self.assertEqual(checkout('A*3B'), -1)
-        self.assertEqual(checkout('A*3'), [['A', 3]])
-        self.assertEqual(checkout('A*1,B*2'), [['A', 1], ['B', 2]])
+        self.assertEqual(checkout('A*3'), -1)
+        self.assertEqual(checkout('A*1,B*2'), -1)
+        self.assertEqual(checkout([''], 0)
+        self.assertEqual(checkout(['A'], 50)
 
 if __name__ == '__main__':
     unittest.main()
